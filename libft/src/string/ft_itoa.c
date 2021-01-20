b868dd969b11659b8fc23cf17021554c438adc3f
Kangkai Ye <yek@MacBook-Pro.local>
2021-01-20 18:54:46 +0100

    BMP -> progressing

--- libft/src/string/ft_itoa.c
+++ libft/src/string/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 16:58:04 by kaye              #+#    #+#             */
-/*   Updated: 2020/11/17 13:03:36 by kaye             ###   ########.fr       */
+/*   Updated: 2021/01/20 18:54:35 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ static size_t	ft_intlen(int n)
 {
 	size_t count;
 
-	count = (n) ? 0 : 1;
+	if (n != 0)
+		count = 0;
+	else
+		count = 1;
 	while (n)
 	{
 		n = n / 10;
@@ -35,7 +38,10 @@ char			*ft_itoa(int n)
 	char			*str;
 	unsigned int	n_tmp;
 
-	n_tmp = (n < 0) ? -(unsigned int)n : n;
+	if (n < 0)
+		n_tmp = -n;
+	else
+		n_tmp = n;
 	len = (n < 0) ? ft_intlen(n_tmp) + 1 : ft_intlen(n_tmp);
 	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);

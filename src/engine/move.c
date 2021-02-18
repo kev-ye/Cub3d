1ba86041ff1738e36726ca7c01d403868b84cb68
Kangkai YE <kaye@e1r9p16.42.fr>
2021-02-18 12:10:05 +0100

    done

--- src/engine/move.c
+++ src/engine/move.c
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/03 17:20:32 by kaye              #+#    #+#             */
-/*   Updated: 2021/02/05 09:36:05 by kaye             ###   ########.fr       */
+/*   Updated: 2021/02/18 12:03:42 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	move_w(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->dir_x
-				* win->camera->speed)] == '0')
+				* win->camera->speed * 2)] == '0')
 		win->camera->pos_x += win->camera->dir_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y + win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0')
 		win->camera->pos_y += win->camera->dir_y * win->camera->speed;
 }
 
@@ -27,10 +27,10 @@ void	move_s(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->dir_x
-				* win->camera->speed)] == '0')
+				* win->camera->speed * 2)] == '0')
 		win->camera->pos_x -= win->camera->dir_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y - win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0')
 		win->camera->pos_y -= win->camera->dir_y * win->camera->speed;
 }
 
@@ -38,10 +38,10 @@ void	move_a(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->plane_x
-				* win->camera->speed)] == '0')
+				* win->camera->speed * 2)] == '0')
 		win->camera->pos_x -= win->camera->plane_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y - win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0')
 		win->camera->pos_y -= win->camera->plane_y * win->camera->speed;
 }
 
@@ -49,9 +49,9 @@ void	move_d(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->plane_x
-				* win->camera->speed)] == '0')
+				* win->camera->speed * 2)] == '0')
 		win->camera->pos_x += win->camera->plane_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y + win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0')
 		win->camera->pos_y += win->camera->plane_y * win->camera->speed;
 }
--- src_bonus/engine/move_bonus.c
+++ src_bonus/engine/move_bonus.c
@@ -6,7 +6,7 @@
 /*   By: kaye <kaye@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/03 17:20:32 by kaye              #+#    #+#             */
-/*   Updated: 2021/02/08 12:38:24 by kaye             ###   ########.fr       */
+/*   Updated: 2021/02/18 12:03:19 by kaye             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@ void	move_w(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->dir_x
-				* win->camera->speed)] == '0' ||
+				* win->camera->speed * 2)] == '0' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->dir_x
-				* win->camera->speed)] == '4' ||
+				* win->camera->speed * 2)] == '4' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->dir_x
-				* win->camera->speed)] == '3')
+				* win->camera->speed * 2)] == '3')
 		win->camera->pos_x += win->camera->dir_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y + win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0' ||
 			win->desc_info->map[(int)(win->camera->pos_y + win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '4' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '4' ||
 			win->desc_info->map[(int)(win->camera->pos_y + win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '3')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '3')
 		win->camera->pos_y += win->camera->dir_y * win->camera->speed;
 }
 
@@ -37,20 +37,20 @@ void	move_s(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->dir_x
-				* win->camera->speed)] == '0' ||
+				* win->camera->speed * 2)] == '0' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->dir_x
-				* win->camera->speed)] == '4' ||
+				* win->camera->speed * 2)] == '4' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->dir_x
-				* win->camera->speed)] == '3')
+				* win->camera->speed * 2)] == '3')
 		win->camera->pos_x -= win->camera->dir_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y - win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0' ||
 			win->desc_info->map[(int)(win->camera->pos_y - win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '4' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '4' ||
 			win->desc_info->map[(int)(win->camera->pos_y - win->camera->dir_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '3')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '3')
 		win->camera->pos_y -= win->camera->dir_y * win->camera->speed;
 }
 
@@ -58,20 +58,20 @@ void	move_a(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->plane_x
-				* win->camera->speed)] == '0' ||
+				* win->camera->speed * 2)] == '0' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->plane_x
-				* win->camera->speed)] == '4' ||
+				* win->camera->speed * 2)] == '4' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x - win->camera->plane_x
-				* win->camera->speed)] == '3')
+				* win->camera->speed * 2)] == '3')
 		win->camera->pos_x -= win->camera->plane_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y - win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0' ||
 			win->desc_info->map[(int)(win->camera->pos_y - win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '4' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '4' ||
 			win->desc_info->map[(int)(win->camera->pos_y - win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '3')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '3')
 		win->camera->pos_y -= win->camera->plane_y * win->camera->speed;
 }
 
@@ -79,19 +79,19 @@ void	move_d(t_win *win)
 {
 	if (win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->plane_x
-				* win->camera->speed)] == '0' ||
+				* win->camera->speed * 2)] == '0' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->plane_x
-				* win->camera->speed)] == '4' ||
+				* win->camera->speed * 2)] == '4' ||
 			win->desc_info->map[(int)win->camera->pos_y]
 			[(int)(win->camera->pos_x + win->camera->plane_x
-				* win->camera->speed)] == '3')
+				* win->camera->speed * 2)] == '3')
 		win->camera->pos_x += win->camera->plane_x * win->camera->speed;
 	if (win->desc_info->map[(int)(win->camera->pos_y + win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '0' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '0' ||
 			win->desc_info->map[(int)(win->camera->pos_y + win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '4' ||
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '4' ||
 			win->desc_info->map[(int)(win->camera->pos_y + win->camera->plane_y
-				* win->camera->speed)][(int)win->camera->pos_x] == '3')
+				* win->camera->speed * 2)][(int)win->camera->pos_x] == '3')
 		win->camera->pos_y += win->camera->plane_y * win->camera->speed;
 }
